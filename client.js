786a57ff164b15a26e3dd2974a1ef9b3616d9185
Sandra Malpica <sandra_malpica@hotmail.com>
2025-06-06 12:51:04 +0200

    Add files via upload

--- client.js
+++ client.js
@@ -10,5 +10,16 @@ window.TrelloPowerUp.initialize({
         });
       }
     }];
+  },
+  'card-back-section': function(t, options) {
+    return [{
+      title: 'Tiempo dedicado',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Clock-icon-blue.png/32px-Clock-icon-blue.png',
+      content: {
+        type: 'iframe',
+        url: 'index.html',
+        height: 300
+      }
+    }];
   }
 });
